380e113a16803d7f4176f5b3cb7f10610c838604
ivanchimenti <ivanchimenti@gmail.com>
2023-04-08 18:30:36 -0300

    Creacion de proyecto

---
+++ test/pb2/unlam/edu/ar/TestCases.java
@@ -0,0 +1,14 @@
+package pb2.unlam.edu.ar;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCases {
+    
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+
+}
