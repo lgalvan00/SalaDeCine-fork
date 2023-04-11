57d7761ae3889c5e623962f002fa0f983a716eac
ivanchimenti <ivanchimenti@gmail.com>
2023-04-10 23:31:36 -0300

    Creación de métodos

--- src/pb2/unlam/edu/ar/SalaDeCine.java
+++ src/pb2/unlam/edu/ar/SalaDeCine.java
@@ -1,5 +1,11 @@
 package pb2.unlam.edu.ar;
 
 public class SalaDeCine {
+	
+	
+	public void ocuparButaca() {}
+	
+	public void consultarCantidadTotalButacasOcupadas() {}
 
+	public void consultarSiSePuedeAcomodarUnGrupoDePersonasJuntas() {}
 }
